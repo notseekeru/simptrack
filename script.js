83be8c42d4e65798b11058df065423475d8a3f9c
Stephen Macabulos <124259804+notseekeru@users.noreply.github.com>
2025-11-01 20:32:11 +0800

    Enhance exercise tracking functionality and update styles
    - Added unique IDs to exercises for better identification.
    - Improved table rendering with dynamic weight adjustment buttons.
    - Updated CSS for table button styles and weight display.
    - Created a README file to describe the project.

--- script.js
+++ script.js
@@ -1,19 +1,62 @@
 import exercises from './workout.js'
 
-
 const tableEl = document.getElementById('table')
+let html = ''
 
 for (const x of exercises) {
     let checkbox = ''
     for (let i = 0; i < x.sets; i++) {
         checkbox += `<input type="checkbox">`
     }
-    tableEl.innerHTML += `
-        <tr>
-        <td>${x.work}</td>
+    html += `
+        <tr data-id="${x.id}">
+        <td>${x.id}. ${x.work}</td>
         <td>${checkbox}</td>
         <td>${x.reps}</td>
-        <td>${x.weight}</td>
+        <td class="weight"><button data-action="minus" data-id="${x.id}">-</button>${x.weight}<button data-action="plus" data-id="${x.id}">+</button></td>
         </tr>
     `
 }
+
+function renderHtml() {
+    let html = ''
+    for (const x of exercises) {
+        let checkbox = ''
+        for (let i = 0; i < x.sets; i++) {
+            checkbox += `<input type="checkbox">`
+        }
+        html += `
+            <tr data-id="${x.id}">
+            <td>${x.id}. ${x.work}</td>
+            <td>${checkbox}</td>
+            <td>${x.reps}</td>
+            <td class="weight">
+                <button data-action="minus" data-id="${x.id - 1}">-</button>
+                ${x.weight}
+                <button data-action="plus" data-id="${x.id - 1}">+</button>
+            </td>
+            </tr>
+        `
+    }
+    tableEl.innerHTML = html
+}
+
+document.addEventListener('click', (e) => {
+    const exerId = e.target.dataset.id
+    switch (e.target.dataset.action) {
+        case 'minus' :
+            if (exercises[exerId].weight > 0) {
+                exercises[exerId].weight -= 5
+                renderHtml()
+            } else {
+                console.log("Already low");
+            }
+            break
+        case 'plus' :
+            exercises[exerId].weight += 5
+            renderHtml()
+            break
+    }
+})
+
+renderHtml()
