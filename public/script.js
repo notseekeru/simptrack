675ddba531ee6b9c4cdc56d1ee27bfa7a243e562
Stephen Macabulos <124259804+notseekeru@users.noreply.github.com>
2025-11-10 00:51:10 +0800

    asdasdasd

--- public/script.js
+++ public/script.js
@@ -1,13 +1,12 @@
 // Import the exercises array from workout.js
-import exercises from './workout.js'
+import gymExercises from './workout.js'
 
 // Get the table element where exercises will be rendered
-const tableEl = document.getElementById('table')
-let html = ''
+
 
 // Check if localStorage already has exercise data; if not, initialize it
 if (!localStorage.getItem("storageSet")) {
-  localStorage.setItem("storageSet", JSON.stringify(exercises))
+    localStorage.setItem("storageSet", JSON.stringify(gymExercises))
 }
 
 // Load exercises from localStorage
@@ -23,7 +22,6 @@ document.addEventListener('click', (e) => {
     const exerId = e.target.dataset.id // Get the exercise id from the button
     switch (e.target.dataset.action) {
         case 'minus' :
-            // Decrease weight by 5 if above 0, then update storage and re-render
             if (storedStorage[exerId].weight > 0) {
                 storedStorage[exerId].weight -= 5
                 saveToLocalStorage()
@@ -33,7 +31,6 @@ document.addEventListener('click', (e) => {
             }
             break
         case 'plus' :
-            // Increase weight by 5, update storage and re-render
             storedStorage[exerId].weight += 5
             saveToLocalStorage()
             renderHtml()
@@ -43,26 +40,27 @@ document.addEventListener('click', (e) => {
 
 // Function to render the exercise table based on storedStorage
 function renderHtml() {
-    let html = ''
+    const tableEl = document.getElementById('table')
+        let html = ``;
     for (const x of storedStorage) {
         let checkbox = ''
         for (let i = 0; i < x.sets; i++) {
             checkbox += `<input type="checkbox">`
         }
-        // Note: data-id uses x.id - 1, which assumes ids are sequential and start at 1
         html += `
             <tr data-id="${x.id}">
-            <td>${x.id}. ${x.work}</td>
-            <td>${checkbox}</td>
-            <td>${x.reps}</td>
-            <td>
-                <button data-action="minus" data-id="${x.id - 1}">-</button>
-                ${x.weight}
-                <button data-action="plus" data-id="${x.id - 1}">+</button>
-            </td>
+                <td>${x.work}</td>
+                <td>${checkbox}</td>
+                <td>${x.reps}</td>
+                <td>
+                    <button data-action="minus" data-id="${x.id}">-</button>
+                    ${x.weight}
+                    <button data-action="plus" data-id="${x.id}">+</button>
+                </td>
             </tr>
         `
     }
+        html += '</tbody>';
     tableEl.innerHTML = html
 }
 
